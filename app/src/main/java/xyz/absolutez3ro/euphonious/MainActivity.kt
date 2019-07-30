017467ca403bc74f41a9de181afdf7684e844160
Shahnawaz Ansari <g0d.d3mi@gmail.com>
2019-07-30 21:24:50 +0530

    Initial Commit.

---
+++ app/src/main/java/xyz/absolutez3ro/euphonious/MainActivity.kt
@@ -0,0 +1,38 @@
+package xyz.absolutez3ro.euphonious
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_dashboard -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_songs -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_albums -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_artists -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_playlist -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+
+
+}
